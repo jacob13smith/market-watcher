835a6d3d6455ec0d96328aafec1bfe833947fb8f
Jacob Smith <jacob13smith@gmail.com>
2019-06-06 03:34:11 -0700

    working stuff

---
+++ src/server/routes/tsx.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+var fs = require('fs');
+var HTMLParser = require('node-html-parser');
+const csv = require('csv-parser')
+
+// This is a test endpoint for framework purposes.
+router.get('/', async( req, res ) => {
+    var symbols = [];
+    fs.createReadStream('./src/server/lib/S&PTSX.csv')
+    .pipe(csv())
+    .on('data', (data) => symbols.push(data))
+    .on('end', () => {
+        var entries = [];
+        var promises = [];
+        for (i = 0; i < symbols.length; i++){
+            let temp = symbols[i].Symbol;
+            promises.push(new Promise(function(resolve, reject) {
+                return axios.all([getStats(temp), getQuote(temp)])
+                .then(axios.spread(function (statsResponse, quoteResponse){
+                    let page = HTMLParser.parse(quoteResponse.data);
+                    let quote = "Not Found";
+                    try {
+                        quote = page.querySelector('span.nasdaqChangeHeader').parentNode.childNodes[6].childNodes[0].rawText.trim();
+                    } catch (err){
+                    }
+                    let root = HTMLParser.parse(statsResponse.data)
+                    let eps = find(root, "Diluted EPS");
+                    let bvps = find(root, "Book Value Per Share");
+                    let eval = eps * bvps;
+                    if (eps == "N/A" || bvps == "N/A"){
+                        eval = "Unknown";
+                    } else if (eval < 0){
+                        eval = "Negative"
+                    } else {
+                        eval = Math.sqrt(22.5 * eps * bvps);
+                    }
+                    resolve({
+                        symbol: temp,
+                        quote: quote,
+                        graham: eval
+                    });
+                }));
+            }));
+        }
+        Promise.all(promises).then(function (responses){
+            fs.writeFile("./output.txt", JSON.stringify(responses));
+            res.json(responses);
+        });
+    });
+});
+
+function find(root, field){
+    var rows = root.querySelectorAll('tr');
+    for (i = 0; i < rows.length; i++){
+        if (rows[i].firstChild.firstChild.text == field){
+            return rows[i].lastChild.text;
+        }
+    }
+}
+
+function getStats(symbol){
+    return axios({
+        method: 'get',
+        url:  `https://ca.finance.yahoo.com/quote/${symbol}.TO/key-statistics?p=${symbol}.TO`
+    });
+}
+
+function getQuote(symbol){
+    symbol = symbol.replace("-","");
+    return axios({
+        method: 'get',
+        url: `https://www.reuters.com/finance/stocks/overview/${symbol}.TO`
+    })
+}
+
+module.exports = router;
